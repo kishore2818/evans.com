33db731ac93568fa3ae2213bf589f12c1501c8ce
Kishore Abinash <kishoreabinash2005@gmail.com>
2026-08-27 13:39:46 +0530

    feat: update favicon to Evans brand logo (crown PNG) for frontend and admin

--- frontend/app/layout.jsx
+++ frontend/app/layout.jsx
@@ -69,8 +69,9 @@ export default function RootLayout({ children }) {
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="apple-mobile-web-app-title" content="Evans Luxe" />
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
-        <link rel="apple-touch-icon" href="/images/logo.jpg" />
+        <link rel="icon" href="/logo.png" type="image/png" sizes="any" />
+        <link rel="shortcut icon" href="/logo.png" type="image/png" />
+        <link rel="apple-touch-icon" href="/logo.png" />
         <link rel="preconnect" href={API_BASE_URL} />
         <link rel="dns-prefetch" href="https://res.cloudinary.com" />
       </head>
